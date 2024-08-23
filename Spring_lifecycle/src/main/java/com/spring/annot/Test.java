79c29834c5c58ae7e06f919d70a80594808edfc1
rm1710 <d290mishraritesh@gmail.com>
2024-08-23 23:03:32 +0530

    spring bean cycle

---
+++ Spring_lifecycle/src/main/java/com/spring/annot/Test.java
@@ -0,0 +1,15 @@
+package com.spring.annot;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.*;
+import org.springframework.context.support.*;
+
+
+public class Test {
+	public static void main(String a[]) {
+		AbstractApplicationContext context=new ClassPathXmlApplicationContext("com/spring/impl/config.xml");
+		context.registerShutdownHook();
+		Student st=context.getBean("st",Student.class);
+		System.out.println(st);
+	}
+}
